292bdf34148e65a367555193362ae34616564ac8
Sharabidinov Alymbek <73030640+sharabidinov@users.noreply.github.com>
2021-11-16 20:56:03 +0600

    исправил

--- hw_5/hw_5_sharabidinov_5.py
+++ hw_5/hw_5_sharabidinov_5.py
@@ -1,7 +1,4 @@
 src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
-
-'''Я реализовал два способа реализации, первая с помощью set, вторая с помощью лист. Разница в том что при реализации 
-через set мы не сможем сохранить последовательность как в списке, но она работает намного быстрее списка. '''
 tmp = set()
 unique_num = set()
 for el in src:
@@ -10,7 +7,5 @@ for el in src:
     else:
         unique_num.discard(el)
     tmp.add(el)
-print(unique_num)
-
-unique_num_lst = [num for num in src if src.count(num) == 1]
-print(unique_num_lst)
+sorted_unique_num =  [el for el in src if el in unique_num]
+print(sorted_unique_num)
